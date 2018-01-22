c4ce8aa7e860b8b35e0838f035bad59fc3d4ef9c
redhope <mrshanksster@gmail.com>
2018-01-21 19:17:11 -0600

    Add game logic.
    Added game logic for all actions: attack, special attack, heal and give up.

--- src/App.vue
+++ src/App.vue
@@ -2,11 +2,13 @@
 .app {
   display: flex;
   justify-content: center;
+  flex-direction: column;
 }
 .player-info {
   display: flex;
   flex-direction: row;
   justify-content: space-between;
+  margin-bottom: 10%;
 }
 
 .player-you {
@@ -33,16 +35,22 @@
              <div class="player-monster">
                   <h1 :style="{textAlign: 'center'}">Monster</h1>
                  <health-bar
+                    :style="{textAlign: 'left'}"
                     :id="'player-2'"
                     :current-health="this.player2Health"></health-bar>
              </div>
          </div>
-         <button-section></button-section>
+         <button-section
+          v-on:attack="attack"
+          v-on:spclAttack="spclAttack"></button-section>
     </div>
 </template>
 
-
 <script>
+function getRandomActionValue(modifier) {
+  return Math.floor(Math.random() * modifier);
+}
+
 import Vue from "vue";
 import HealthBar from "./HealthBar.vue";
 import ButtonSection from "./ButtonSection.vue";
@@ -52,9 +60,50 @@ export default {
   data() {
     return {
       message: "Hello Vue",
-      player1Health: 20,
-      player2Health: 50
+      player1Health: 100,
+      player2Health: 100,
+      actionModifier: 10
     };
+  },
+  methods: {
+    startGame() {
+      this.isGameInProgress = true;
+    },
+    attack() {
+      let playerAttack, monsterAttack;
+
+      playerAttack = getRandomActionValue(this.actionModifier);
+      monsterAttack = getRandomActionValue(this.actionModifier);
+      this.modifyPlayerHealthValues(-monsterAttack, -playerAttack);
+    },
+    spclAttack() {
+      let playerAttack, monsterAttack;
+
+      playerAttack = getRandomActionValue(this.actionModifier * 2);
+      monsterAttack = getRandomActionValue(this.actionModifier);
+      this.modifyPlayerHealthValues(-monsterAttack, -playerAttack);
+    },
+    healSelf() {
+      player1Delta = getRandomActionValue(this.actionModifier) - getRandomActionValue(this.actionModifier * 0.8);
+      this.modifyPlayerHealthValues(player1Delta, 0);
+    },
+    endGame() {
+      this.isGameInProgress = false;
+      this.player1Health = 100;
+      this.player2Health = 100;
+    },
+    modifyPlayerHealthValues(player1Delta, player2Delta) {
+      this.player1Health += player1Delta;
+      this.player2Health += player2Delta;
+    }
+  },
+  computed: {
+    hasGameEnded() {
+      return (this.player1Health <=0) || this.player2Health <=0;
+    },
+    isPlayer1Winner() {
+      return this.player1Health >=0;
+    }
   },
   components: {
     HealthBar,
--- src/ButtonSection.vue
+++ src/ButtonSection.vue
@@ -1,23 +1,86 @@
 <template>
-  <section class="button-section">
-    <div></div>
-  </section>
+  <div class="button-section">
+    <div v-if="!isGameInProgress" class="btn-group">
+      <button class="btn" @click="startGame">START NEW GAME</button>
+    </div>
+    <div v-else class="btn-group">
+      <button class="btn btn-attack" @click="attack">ATTACK</button>
+      <button class="btn btn-spcl" @click="spclAttack">SPECIAL ATTACK</button>
+      <button class="btn btn-heal" @click="healSelf">HEAL</button>
+      <button class="btn btn-lose" @click="endGame">GIVE UP</button>
+    </div>
+  </div>
 </template>
 
 <script>
 export default {
-  name: 'button-section',
+  name: "button-section",
   data() {
-    return {}
+    return {
+      isGameInProgress: false,
+      actionModifier: 50
+    };
+  },
+  methods: {
+    startGame() {
+      this.isGameInProgress = true;
+      this.$emit('startGame');
+    },
+    attack() {
+      // let playerAttack, monsterAttack;
+
+      // playerAttack = getRandomActionValue(this.actionModifier);
+      // monsterAttack = getRandomActionValue(this.actionModifier);
+      this.$emit('attack');
+    },
+    spclAttack() {
+      this.$emit('spclAttack');      
+    },
+    healSelf() {
+      this.$emit('healSelf');      
+    },
+    endGame() {
+      this.isGameInProgress = false;
+      this.$emit('endGame');      
+    }
   }
-}
+};
 </script>
 
 <style scoped>
 .button-section {
   border: 1px solid gray;
-  border-radius: 10%;
-  box-shadow: 3px;
-  width: 100%;
+  border-radius: 5px;
+  box-shadow: 0px 0px 4px 0px black;
+  width: 80%;
+  margin: 0 auto;
+}
+
+.btn-group {
+  display: flex;
+  justify-content: center;
+  padding: 10px;
+}
+
+.btn {
+  background-color: lightgreen;
+  padding: 10px;
+  font-size: 14px;
+  font-weight: 600;
+  border-radius: 5px;
+  box-shadow: 0 0 3px 1px black;
+  margin: auto;
+}
+
+.btn-attack {
+  background-color: orangered;
+}
+
+.btn-spcl {
+  background-color: peru;
+}
+
+.btn-lose {
+  background-color: whitesmoke;
 }
 </style>
