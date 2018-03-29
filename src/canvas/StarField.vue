2d25864114e16300f1da2f24bc3646b2fc4bfd96
Anurag Sharma <c.anurag.sharma@uptake.com>
2018-03-28 21:01:18 -0500

    Add starfield component skeleton

---
+++ src/canvas/StarField.vue
@@ -0,0 +1,31 @@
+<template>
+  
+</template>
+
+<script>
+export default {
+  name: 'star-field',
+  data() {
+    return {};
+  },
+  props: {
+    screenSize: Number,
+    size: Number
+  },
+  mounted() {
+    var vm = this;
+    setInterval(() => {
+      vm.move(this.size);
+    }, 100);
+  },
+  methods: {
+      move(size) {
+          
+      }
+  }
+};
+</script>
+
+<style>
+
+</style>
