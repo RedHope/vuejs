0fb711db86fad9cdebe5ff595d9d4652fdadb907
Anurag Sharma <mrshanksster@gmail.com>
2018-03-29 20:03:10 -0500

    Add code setup to prepare for transitions

--- src/main.js
+++ src/main.js
@@ -1,5 +1,5 @@
 import Vue from 'vue';
-import App from './project-4-sol/App.vue';
+import App from './animations/App.vue';
 
 Vue.directive('translate', function(el) {
   el.innerHTML = 'translated';
