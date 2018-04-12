620659de12f5139f063d40c71855ddf0c9808dd1
Anurag Sharma <mrshanksster@gmail.com>
2018-04-11 21:35:31 -0500

    Navigating via JS code, using this.$router

--- src/routing/user/User.vue
+++ src/routing/user/User.vue
@@ -1,11 +1,21 @@
 <template>
-  <h1>The User page</h1>
+  <div>
+    <h1>The User page</h1>
+    <hr>
+    <button class="btn btn-primary" @click="goHome">Go to Home</button>
+  </div>
 </template>
 
 <script>
 export default {
-
-}
+  methods: {
+    goHome() {
+      this.$router.push({
+        path: '/'
+      });
+    }
+  }
+};
 </script>
 
 <style>
