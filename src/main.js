64cd19c633f9ecbb957507da4150dc9d3002de29
Anurag Sharma <mrshanksster@gmail.com>
2018-04-11 21:24:37 -0500

    Add active states to routes.

--- src/main.js
+++ src/main.js
@@ -8,20 +8,21 @@ import { routes } from './routing/routes';
 Vue.use(VueResource);
 Vue.http.options.root = `${ENVIRONMENT.FIREBASE_URL}/`;
 Vue.http.interceptors.push((req, next) => {
-    next(res => {
-        res.json = () => res.body;
-    });
+  next(res => {
+    res.json = () => res.body;
+  });
 });
 Vue.directive('translate', function(el) {
-    el.innerHTML = 'translated';
+  el.innerHTML = 'translated';
 });
 Vue.use(VueRouter);
 
 const router = new VueRouter({
-    routes
+  routes,
+  mode: 'history'
 });
 new Vue({
-    el: '#app',
-    router,
-    render: h => h(App)
+  el: '#app',
+  router,
+  render: h => h(App)
 });
