05873665e5069e3815e73ec369e84469607db8e8
Anurag Sharma <mrshanksster@gmail.com>
2018-05-16 21:08:14 -0500

    Skip routing for now, moving on to VueX

--- src/routing/user/UserDetail.vue
+++ src/routing/user/UserDetail.vue
@@ -2,15 +2,19 @@
   <div>
     <h3>Some user details</h3>
     <p>User loaded: {{$route.params.id}}</p>
+    <router-link :to="route" class="btn btn-primary">Edit User</router-link>
   </div>
 </template>
 
 <script>
 export default {
-
-}
+    computed: {
+        route() {
+            return `/user/${this.$route.params.id}/edit`;
+        }
+    }
+};
 </script>
 
 <style>
-
 </style>
