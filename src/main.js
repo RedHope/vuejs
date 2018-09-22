1c0006b9cc6579e077c1092e74a422a7d2e10aec
Anurag Sharma <c.anurag.sharma@uptake.com>
2018-09-22 09:43:25 -0500

    vuex: setting up vuex store

--- src/main.js
+++ src/main.js
@@ -1,28 +1,27 @@
 import Vue from 'vue';
-import VueResource from 'vue-resource';
-import VueRouter from 'vue-router';
-import App from './project-4-sol/App.vue';
+import App from './vuex/App.vue';
+import store from './vuex/store';
 
-import { routes } from './routing/routes';
 
-Vue.use(VueResource);
-Vue.http.options.root = `${ENVIRONMENT.FIREBASE_URL}/`;
-Vue.http.interceptors.push((req, next) => {
-  next(res => {
-    res.json = () => res.body;
-  });
-});
+// Vue.use(VueResource);
+// Vue.http.options.root = `${ENVIRONMENT.FIREBASE_URL}/`;
+// Vue.http.interceptors.push((req, next) => {
+//   next(res => {
+//     res.json = () => res.body;
+//   });
+// });
 Vue.directive('translate', function(el) {
   el.innerHTML = 'translated';
 });
-Vue.use(VueRouter);
+// Vue.use(VueRouter);
 
-const router = new VueRouter({
-  routes,
-  mode: 'history'
-});
+// const router = new VueRouter({
+//   routes,
+//   mode: 'history'
+// });
 new Vue({
+  store,
   el: '#app',
-  router,
+  // router,
   render: h => h(App)
 });
